0757f3eaec3bcbe9148c30b191acf9fdab8a770b
goldcode101 <chris_lnz@yahoo.com>
2021-03-14 10:50:37 -0400

    Add the Procfile for Heroku and allow CORS

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ const cars = [
     }
 ]
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/api/cars', (req, res) => {
     res.json(cars);
 });
