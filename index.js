ab3a1cfa691e87765d64f7f7b4b38fb27976b945
goldcode101 <chris_lnz@yahoo.com>
2024-11-16 12:03:57 -0500

    Implementing a basic HTML page, and cors for index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const cars = [
     {
